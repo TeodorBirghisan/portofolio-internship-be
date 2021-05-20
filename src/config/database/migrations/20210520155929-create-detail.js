d274318ffe2941823e2d551ca9f29f9aad094509
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-05-20 19:02:14 +0300

    created and applied migration for detail

---
+++ src/config/database/migrations/20210520155929-create-detail.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Details', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      location: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      startyear: {
+        type: Sequelize.INTEGER
+      },
+      endyear: {
+        type: Sequelize.INTEGER
+      },
+      numberOfMonths: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Details');
+  }
+};
