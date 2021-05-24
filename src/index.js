1bc6ae7b9c03cb8165a89d959a4d742f12070445
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-05-24 13:01:15 +0300

    setup for web requests and routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const app = require('express')();
 const cors = require('cors');
+const express = require('express');
 require('dotenv').config();
-const db = require('./api/models');
+const routes = require('./api/routes');
 const port = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cors());
+
+routes(app);
 
 app.listen(port, async () => {
   try {
