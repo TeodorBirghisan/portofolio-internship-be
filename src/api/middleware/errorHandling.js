fcb7d576df395af1b6e7da5bf0f066fdc976671f
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-05-24 18:48:18 +0300

    created and applied errorHandling middlewares

---
+++ src/api/middleware/errorHandling.js
@@ -0,0 +1,21 @@
+function notFound(req, res, next) {
+  res.status(404);
+  const error = new Error(`🔍 - Not Found - ${req.originalUrl}`);
+  next(error);
+}
+
+/* eslint-disable no-unused-vars */
+function errorHandler(err, req, res, next) {
+  /* eslint-enable no-unused-vars */
+  const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? 'prod' : err.stack,
+  });
+}
+
+module.exports = {
+  notFound,
+  errorHandler,
+};
--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ const express = require('express');
 require('dotenv').config();
 const routes = require('./api/routes');
 const port = process.env.PORT || 3000;
+require('./api/middleware/passport');
+const middleware = require('./api/middleware/errorHandling');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
 routes(app);
+
+app.use(middleware.notFound);
+app.use(middleware.errorHandler);
 
 app.listen(port, async () => {
   try {
