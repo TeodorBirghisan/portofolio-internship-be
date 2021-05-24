f4e1f97f512644a679383dc4e048b8ec956d8150
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-05-24 11:54:34 +0300

    use cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const app = require('express')();
+const cors = require('cors');
 require('dotenv').config();
-
+const db = require('./api/models');
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.listen(port, async () => {
   try {
     console.log(`Listening on port ${port}`);
+    //console.log(await db.Skill.findAll({ where: { userId: 1 } }));
   } catch (error) {
     console.error(error);
     throw new Error(error);
