e6b1c7896ee8a5cd95e00ea04065bdd16b6de7b3
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-05-24 13:51:15 +0300

    include details to ausbildung

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -8,10 +8,8 @@ router.get('/:id', async (req, res) => {
     const userSkills = await db.Skill.findOne({ where: { userId: id } });
     const userAusbildung = await db.Ausbildung.findAll({
       where: { userId: id },
+      include: db.Details,
     });
-    //TODO: a user has many ausbildungs -> a detail has n ausbildungsId-> getAll details for the different ausbildungs that a user has
-    //const ausbildungId = userAusbildung.dataValues.id;
-    //const userDetail = await db.Detail.findOne({ where: { ausbildungId } });
     return res.status(200).json({ user, userSkills, userAusbildung });
   } catch (error) {
     console.log(error);
