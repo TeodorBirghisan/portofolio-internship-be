f3e81fd1a92f384aa0cac3491806df56bceeb038
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-05-24 18:47:11 +0300

    fixed type mistake

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -8,7 +8,7 @@ router.get('/:id', async (req, res) => {
     const userSkills = await db.Skill.findOne({ where: { userId: id } });
     const userAusbildung = await db.Ausbildung.findAll({
       where: { userId: id },
-      include: db.Details,
+      include: db.Detail,
     });
     return res.status(200).json({ user, userSkills, userAusbildung });
   } catch (error) {
