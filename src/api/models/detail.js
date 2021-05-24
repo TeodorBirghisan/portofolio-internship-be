a4c12f538d4f9c05659e5c436a925cb04541221d
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-05-24 13:00:49 +0300

    Fixed relationship bug

--- src/api/models/detail.js
+++ src/api/models/detail.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Detail.hasOne(models.Ausbildung, {
+      Detail.belongsTo(models.Ausbildung, {
         foreignKey: 'ausbildungId',
         onDelete: 'CASCADE',
       });
