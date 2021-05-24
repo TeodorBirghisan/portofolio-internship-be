8e89f43e94085f0d5d4708e6bd1bb91524a14961
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-05-24 18:11:58 +0300

    created login route

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,6 +1,8 @@
 const userRouter = require('./user');
 const registerRouter = require('./register');
+const loginRouter = require('./login');
 module.exports = (app) => {
   app.use('/user', userRouter);
   app.use(registerRouter);
+  app.use(loginRouter);
 };
