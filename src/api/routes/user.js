e5904c59a67fca7c31b2a90332f4c54cc0705fe0
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-05-25 20:25:30 +0300

    use passport for authentication in user route

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -4,19 +4,23 @@ const _ = require('lodash');
 const passport = require('passport');
 //TODO: add the line below as middleware for use of jwt auth
 //passport.authenticate('jwt', { session: false }),
-router.get('/:id', async (req, res) => {
-  try {
-    const { id } = _.get(req, 'params', null);
-    const user = await db.User.findByPk(id);
-    const userSkills = await db.Skill.findOne({ where: { userId: id } });
-    const userAusbildung = await db.Ausbildung.findAll({
-      where: { userId: id },
-      include: db.Detail,
-    });
-    return res.status(200).json({ user, userSkills, userAusbildung });
-  } catch (error) {
-    console.log(error);
+router.get(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res) => {
+    try {
+      const { id } = _.get(req, 'params', null);
+      const user = await db.User.findByPk(id);
+      const userSkills = await db.Skill.findOne({ where: { userId: id } });
+      const userAusbildung = await db.Ausbildung.findAll({
+        where: { userId: id },
+        include: db.Detail,
+      });
+      return res.status(200).json({ user, userSkills, userAusbildung });
+    } catch (error) {
+      console.log(error);
+    }
   }
-});
+);
 
 module.exports = router;
