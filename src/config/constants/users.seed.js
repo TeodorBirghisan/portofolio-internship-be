4899d8d78eb2fb53f5b4d36361100abd7b301008
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-05-30 19:08:31 +0300

    new line from styling

--- src/config/constants/users.seed.js
+++ src/config/constants/users.seed.js
@@ -9,7 +9,7 @@ exports.users = [
     hobby:
       'Pe langa hobby-ul, care imi ocupa majoritatea timpului, programarea, imi place in timpul liber sa fac activitati care sunt total opuse. Spre exemplu, iarna, incerc sa merg la ski cu fiecare ocazie, vara incerc trasee cu bicicleta pana unu dintre noi se rupe, sau o excursie de cateva zile. Cand vremea nu imi permite astfel de activitati, prefer sa citesc sau eventual sa ma joc pe PC. Totodata, incerc pe cat posibil sa nu ratez petreceri sau evenimente, care mi se par mai speciale sau care mi-ar aduce o experienta noua.',
     telephone: '0727883745',
-    linkedIn: 'https://www.linkedin.com/in/teodor-birghisan-5585881b8/',
+    linkedIn: 'https://www.linkedin.com/in/teodorcd-birghisan-5585881b8/',
     password: '1234',
     createdAt: new Date(),
     updatedAt: new Date(),
@@ -21,7 +21,8 @@ exports.users = [
     email: 'puchianu_alexandra@yahoo.com',
     age: 22,
     occupation: 'Software Developer',
-    hobby: 'In timpul liber imi place sa descopar noi pasiuni. Printre hobby-urile mele se enumara si dansul, drumetiile, schiatul, mersul cu bicicleta si multe altele.',
+    hobby:
+      'In timpul liber imi place sa descopar noi pasiuni. Printre hobby-urile mele se enumara si dansul, drumetiile, schiatul, mersul cu bicicleta si multe altele.',
     telephone: '0749816360',
     linkedIn: 'https://www.linkedin.com/in/alexandra-puchianu-433a381b4/',
     password: '1234',
